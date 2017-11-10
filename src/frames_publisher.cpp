521a2fcf0b4ee1948dad83653927a0dfef1672dd
uladbohdan <vlbohdan@gmail.com>
2017-11-10 17:14:49 +0200

    images_utils init commit; publishing frames over ROS

---
+++ src/frames_publisher.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <boost/filesystem.hpp>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "image_publisher");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+    std::vector<std::string> images;
+
+    std::set<std::string> available_extensions{".png", ".PNG", ".jpg", ".JPG", ".jpeg", ".JPEG"};
+
+    boost::filesystem::path path(argv[1]);
+    boost::filesystem::directory_iterator iter{path};
+    while (iter != boost::filesystem::directory_iterator{}) {
+        if (available_extensions.find(iter->path().extension().string()) != available_extensions.end()) {
+            images.push_back(iter->path().string());
+        }
+        iter++;
+    }
+
+    std::cout << "Number of images found: " << images.size() << std::endl;
+    if (images.empty()) {
+        return 0;
+    }
+    std::sort(images.begin(), images.end());
+    // TODO(uladbohdan): to check if conversion is successful.
+    ros::Rate loop_rate(atoi(argv[2]));
+    int pos = 0;
+    while (nh.ok()) {
+        pos %= images.size();
+        cv::Mat image = cv::imread(images[pos], CV_LOAD_IMAGE_COLOR);
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+
+        pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+        pos++;
+    }
+}
+
